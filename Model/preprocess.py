ce6e24b8d4be74e04b42c51a22432af64456aa0f
ItIsMe-H <114284506+ItIsMe-H@users.noreply.github.com>
2024-09-17 22:28:09 +0300

    Updated preprocess.py and train_model.py

--- Model/preprocess.py
+++ Model/preprocess.py
@@ -10,12 +10,17 @@ inverse_vocab = {0: UNK_TOKEN, 1: PAD_TOKEN}
 
 def build_vocab(data_file):
     global vocab, inverse_vocab
-    with open(data_file, 'r') as file:
-        data = json.load(file)
+    try:
+        with open(data_file, 'r') as file:
+            data = json.load(file)
+    except FileNotFoundError:
+        raise Exception(f"Data file {data_file} not found.")
+    except json.JSONDecodeError:
+        raise Exception(f"Error decoding JSON from {data_file}.")
     
     for item in data:
-        command = item['command']
-        response = item['response']
+        command = item.get('command', '')
+        response = item.get('response', '')
         for text in [command, response]:
             for word in text.split():
                 if word not in vocab:
@@ -27,27 +32,33 @@ def tokenize(text, vocab):
     return [vocab.get(word, vocab[UNK_TOKEN]) for word in text.split()]
 
 def pad_sequence(sequence, max_len, pad_token_id):
-    return sequence + [pad_token_id] * (max_len - len(sequence))
+    return sequence[:max_len] + [pad_token_id] * (max_len - len(sequence))
 
 class TextDataset(Dataset):
-    def __init__(self, data_file):
-        # Build vocabulary from dataset
+    def __init__(self, data_file, max_len=50):
+        # Build vocab and load data
         build_vocab(data_file)
-        
-        # Load data
+        self.data = []
         with open(data_file, 'r') as file:
-            self.data = json.load(file)
-        
-        # Determine max sequence length
-        self.max_len = max(max(len(tokenize(item['command'], vocab)), len(tokenize(item['response'], vocab))) for item in self.data)
+            self.raw_data = json.load(file)
 
+        for item in self.raw_data:
+            command = tokenize(item['command'], vocab)
+            response = tokenize(item['response'], vocab)
+            command = pad_sequence(command, max_len, vocab[PAD_TOKEN])
+            response = pad_sequence(response, max_len, vocab[PAD_TOKEN])
+            self.data.append((command, response))
+
+        self.vocab = vocab
+        self.pad_token = vocab[PAD_TOKEN]
+    
     def __len__(self):
         return len(self.data)
-
+    
     def __getitem__(self, idx):
-        item = self.data[idx]
-        command = item['command']
-        response = item['response']
-        command_tensor = torch.tensor(pad_sequence(tokenize(command, vocab), self.max_len, vocab[PAD_TOKEN]), dtype=torch.long)
-        response_tensor = torch.tensor(pad_sequence(tokenize(response, vocab), self.max_len, vocab[PAD_TOKEN]), dtype=torch.long)
-        return command_tensor, response_tensor
+        return torch.tensor(self.data[idx][0]), torch.tensor(self.data[idx][1])
+
+# Collate function for DataLoader
+def collate_fn(batch):
+    src, tgt = zip(*batch)
+    return torch.stack(src), torch.stack(tgt)
--- Model/train_model.py
+++ Model/train_model.py
@@ -1,32 +1,44 @@
 import torch
 import torch.optim as optim
 from model import SimpleTransformer
-from preprocess import TextDataset
+from preprocess import TextDataset, collate_fn
+import os
 
-def train_model():
+def train_model(data_file='Data/commands_responses.json', save_path='model_checkpoint.pth'):
+    # Check if CUDA is available
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    
     # Initialize dataset and dataloader
-    dataset = TextDataset('./data/commands_responses.json')
+    dataset = TextDataset(data_file)
     dataloader = torch.utils.data.DataLoader(dataset, batch_size=2, shuffle=True, collate_fn=collate_fn)
-
+    
     # Initialize model
     model = SimpleTransformer(vocab_size=len(dataset.vocab), d_model=512, nhead=8, num_encoder_layers=6, num_decoder_layers=6)
+    model.to(device)
+    
     optimizer = optim.Adam(model.parameters(), lr=0.001)
-    criterion = torch.nn.CrossEntropyLoss(ignore_index=dataset.vocab[dataset.PAD_TOKEN])  # Use PAD token id from dataset
-
-    for epoch in range(10):  # Number of epochs
+    criterion = torch.nn.CrossEntropyLoss(ignore_index=dataset.pad_token)
+    
+    # Training loop
+    num_epochs = 10
+    for epoch in range(num_epochs):
+        model.train()
+        total_loss = 0
         for src, tgt in dataloader:
+            src, tgt = src.to(device), tgt.to(device)
             optimizer.zero_grad()
             output = model(src, tgt)
             loss = criterion(output.view(-1, output.size(-1)), tgt.view(-1))
             loss.backward()
             optimizer.step()
-            print(f'Epoch {epoch}, Loss: {loss.item()}')
-
-def collate_fn(batch):
-    src_batch, tgt_batch = zip(*batch)
-    src_batch = torch.nn.utils.rnn.pad_sequence(src_batch, batch_first=True)
-    tgt_batch = torch.nn.utils.rnn.pad_sequence(tgt_batch, batch_first=True)
-    return src_batch, tgt_batch
-
-if __name__ == "__main__":
-    train_model()
+            total_loss += loss.item()
+        
+        avg_loss = total_loss / len(dataloader)
+        print(f'Epoch {epoch + 1}/{num_epochs}, Loss: {avg_loss:.4f}')
+        
+        # Save model checkpoint
+        if (epoch + 1) % 5 == 0:
+            torch.save(model.state_dict(), save_path)
+            print(f'Model checkpoint saved at {save_path}')
+    
+    print("Training completed.")
