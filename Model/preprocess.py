38b31c69aac1434ecb55e22d3b73e7df8296ee75
ItIsMe-H <114284506+ItIsMe-H@users.noreply.github.com>
2024-09-17 20:31:19 +0300

    Describe your changes here

--- Model/preprocess.py
+++ Model/preprocess.py
@@ -1,56 +1,53 @@
 import json
 import torch
 from torch.utils.data import Dataset
-from torch.nn.utils.rnn import pad_sequence
-from collections import Counter
-from typing import List
+
+# Define special tokens and a default vocabulary
+UNK_TOKEN = '<unk>'
+PAD_TOKEN = '<pad>'
+vocab = {UNK_TOKEN: 0, PAD_TOKEN: 1}
+inverse_vocab = {0: UNK_TOKEN, 1: PAD_TOKEN}
+
+def build_vocab(data_file):
+    global vocab, inverse_vocab
+    with open(data_file, 'r') as file:
+        data = json.load(file)
+    
+    for item in data:
+        command = item['command']
+        response = item['response']
+        for text in [command, response]:
+            for word in text.split():
+                if word not in vocab:
+                    index = len(vocab)
+                    vocab[word] = index
+                    inverse_vocab[index] = word
+
+def tokenize(text, vocab):
+    return [vocab.get(word, vocab[UNK_TOKEN]) for word in text.split()]
+
+def pad_sequence(sequence, max_len, pad_token_id):
+    return sequence + [pad_token_id] * (max_len - len(sequence))
 
 class TextDataset(Dataset):
     def __init__(self, data_file):
+        # Build vocabulary from dataset
+        build_vocab(data_file)
+        
+        # Load data
         with open(data_file, 'r') as file:
             self.data = json.load(file)
         
-        # Create vocabulary and token mappings
-        self.vocab = self.build_vocab()
-        self.pad_token_id = self.vocab['<pad>']
-        
-    def build_vocab(self) -> dict:
-        # Example vocabulary
-        vocab = {'<pad>': 0}
-        counter = Counter()
-        
-        for item in self.data:
-            command = item['command']
-            response = item['response']
-            counter.update(command.split())
-            counter.update(response.split())
-        
-        for word, _ in counter.items():
-            if word not in vocab:
-                vocab[word] = len(vocab)
-        
-        return vocab
+        # Determine max sequence length
+        self.max_len = max(max(len(tokenize(item['command'], vocab)), len(tokenize(item['response'], vocab))) for item in self.data)
 
     def __len__(self):
         return len(self.data)
 
     def __getitem__(self, idx):
         item = self.data[idx]
-        command = item['command'].split()
-        response = item['response'].split()
-        
-        command_tensor = torch.tensor([self.vocab.get(word, 0) for word in command], dtype=torch.long)
-        response_tensor = torch.tensor([self.vocab.get(word, 0) for word in response], dtype=torch.long)
-        
+        command = item['command']
+        response = item['response']
+        command_tensor = torch.tensor(pad_sequence(tokenize(command, vocab), self.max_len, vocab[PAD_TOKEN]), dtype=torch.long)
+        response_tensor = torch.tensor(pad_sequence(tokenize(response, vocab), self.max_len, vocab[PAD_TOKEN]), dtype=torch.long)
         return command_tensor, response_tensor
-
-def collate_fn(batch: List[tuple]) -> tuple:
-    src_batch, tgt_batch = zip(*batch)
-    
-    src_lengths = [len(src) for src in src_batch]
-    tgt_lengths = [len(tgt) for tgt in tgt_batch]
-    
-    src_padded = pad_sequence(src_batch, batch_first=True, padding_value=0)
-    tgt_padded = pad_sequence(tgt_batch, batch_first=True, padding_value=0)
-    
-    return src_padded, tgt_padded, src_lengths, tgt_lengths
--- Model/train_model.py
+++ Model/train_model.py
@@ -1,39 +1,32 @@
 import torch
-import torch.nn as nn
 import torch.optim as optim
-from torch.utils.data import DataLoader
-from preprocess import TextDataset, collate_fn
 from model import SimpleTransformer
+from preprocess import TextDataset
 
 def train_model():
-    # Load dataset
+    # Initialize dataset and dataloader
     dataset = TextDataset('data/commands_responses.json')
-    dataloader = DataLoader(dataset, batch_size=2, shuffle=True, collate_fn=collate_fn)
+    dataloader = torch.utils.data.DataLoader(dataset, batch_size=2, shuffle=True, collate_fn=collate_fn)
 
-    # Model parameters
-    vocab_size = len(dataset.vocab)
-    d_model = 512
-    nhead = 8
-    num_encoder_layers = 6
-    num_decoder_layers = 6
-    model = SimpleTransformer(vocab_size, d_model, nhead, num_encoder_layers, num_decoder_layers)
-
-    # Training parameters
-    criterion = nn.CrossEntropyLoss(ignore_index=dataset.pad_token_id)
+    # Initialize model
+    model = SimpleTransformer(vocab_size=len(dataset.vocab), d_model=512, nhead=8, num_encoder_layers=6, num_decoder_layers=6)
     optimizer = optim.Adam(model.parameters(), lr=0.001)
+    criterion = torch.nn.CrossEntropyLoss(ignore_index=dataset.vocab[dataset.PAD_TOKEN])  # Use PAD token id from dataset
 
-    # Training loop
-    model.train()
-    for epoch in range(1):  # Change the range for more epochs
-        for src, tgt, src_lengths, tgt_lengths in dataloader:
-            # Forward pass
+    for epoch in range(10):  # Number of epochs
+        for src, tgt in dataloader:
             optimizer.zero_grad()
-            output = model(src, tgt[:, :-1])  # Adjust tgt to match transformer expectations
-            loss = criterion(output.view(-1, vocab_size), tgt[:, 1:].contiguous().view(-1))
-            # Backward pass and optimization
+            output = model(src, tgt)
+            loss = criterion(output.view(-1, output.size(-1)), tgt.view(-1))
             loss.backward()
             optimizer.step()
-            print(f"Epoch {epoch+1}, Loss: {loss.item()}")
+            print(f'Epoch {epoch}, Loss: {loss.item()}')
+
+def collate_fn(batch):
+    src_batch, tgt_batch = zip(*batch)
+    src_batch = torch.nn.utils.rnn.pad_sequence(src_batch, batch_first=True)
+    tgt_batch = torch.nn.utils.rnn.pad_sequence(tgt_batch, batch_first=True)
+    return src_batch, tgt_batch
 
 if __name__ == "__main__":
     train_model()
